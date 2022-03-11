d781f580ffdbee126692efb7c9502a1aa8cd936f
Emily Wilbourn <ewilbourn2111@gmail.com>
2022-03-11 11:53:41 -0500

    removed unnecessary print statements

--- smartshuffle.py
+++ smartshuffle.py
@@ -2,16 +2,13 @@ from hashlib import new
 import random
 
 #levels = [5, 3, 1, 2, 4]
-#levels = [5, 1, 4, 2, 3]
-levels = [10, 1, 8, 9, 2, 7, 4, 6, 3, 5]
+levels = [5, 1, 4, 2, 3]
+#levels = [10, 1, 8, 9, 2, 7, 4, 6, 3, 5]
 
 highestLevel = max(levels)
 diff = highestLevel//2
 
-#for i in range(len(levels)):
 while len(levels) > 0:
-    #print("LIST AT START: ", levels)
-    #print("diff: ", diff)
     currentSong = levels[0]
     if 2 != len(levels):
         nextSong = levels[1]
@@ -22,7 +19,6 @@ while len(levels) > 0:
 
     #check difference in levels in current and next song
     newdiff = abs(nextSong-currentSong)
-    #print("newdiff: ", newdiff)
     if newdiff <= diff:
         #pop off the current song from front of list
         print(levels[0])
@@ -35,13 +31,8 @@ while len(levels) > 0:
         lowerIndex = (currentSong-diff) if (currentSong-diff) >= 0  else 1
         upperIndex = (currentSong+diff) if (currentSong+diff) <= highestLevel else highestLevel
 
-        #print("lower: ", lowerIndex)
-        #print("upper: ", upperIndex)
-
         #generate random level - gets the level of a song to grab
         newNextSong = random.randint(lowerIndex, upperIndex)
-        #print("list before newSong: ", levels)
-        #print("newNextSong: ", newNextSong)
 
         #find a song of this level in the list
         index = levels.index(newNextSong) if newNextSong in levels[1:] else -1 
@@ -50,11 +41,8 @@ while len(levels) > 0:
                 newNextSong = random.randint(lowerIndex, upperIndex)
                 index = levels.index(newNextSong) if newNextSong in levels[1:] else -1 
 
-        #print("newNextSong: ", newNextSong)
         levels.insert(1, levels[index])
-        #print("list before: ", levels)
         levels.pop(index+1)
-        #print("list after: ", levels)
         print(levels[0])
         levels.pop(0)
 print(levels[0])
